a6aabbe1a0899376117130a4fed4b2d15e4bf079
Urvesh109 <urveshi.t@gmail.com>
2024-02-03 21:45:53 +0530

    Add test-case for mintTo and transferCheckedWithFee ixs

--- tests/transferFee.ts
+++ tests/transferFee.ts
@@ -5,6 +5,7 @@ import {
   TOKEN_2022_PROGRAM_ID,
   ExtensionType,
   getMintLen,
+  createAssociatedTokenAccountIdempotent,
 } from "@solana/spl-token";
 import * as path from "path";
 import { keypairFromFile, runTest, sendAndConfirmTransaction } from "./utils";
@@ -17,19 +18,23 @@ describe("token-extension transfer-fee", () => {
   const program = anchor.workspace.TokenExtension as Program<TokenExtension>;
 
   const admin = keypairFromFile(path.join(__dirname, "../keypairs/admin.json"));
+  const receiver = keypairFromFile(
+    path.join(__dirname, "../keypairs/receiver.json")
+  );
   const mint = anchor.web3.Keypair.generate();
   console.log("Mint", mint.publicKey.toBase58());
 
   it(
     "Set transferFeeConfig",
     runTest(async () => {
+      const feeBasisPoint = 50;
       const tx = await program.methods
         .transferFeeConfig(
           new anchor.BN(getMintLen([ExtensionType.TransferFeeConfig])),
           admin.publicKey,
           admin.publicKey,
-          50,
-          new anchor.BN(1000)
+          feeBasisPoint,
+          new anchor.BN(100).mul(new anchor.BN(10).pow(new anchor.BN(2)))
         )
         .accounts({
           mint: mint.publicKey,
@@ -41,12 +46,80 @@ describe("token-extension transfer-fee", () => {
         })
         .transaction();
 
-      const txId = await sendAndConfirmTransaction({
+      await sendAndConfirmTransaction({
         connection: provider.connection,
         transaction: tx,
         signers: [admin, mint],
       });
-      console.log("txId", txId);
+
+      const associatedTokenAcc = await createAssociatedTokenAccountIdempotent(
+        provider.connection,
+        admin,
+        mint.publicKey,
+        admin.publicKey,
+        { commitment: "processed" },
+        TOKEN_2022_PROGRAM_ID
+      );
+
+      console.log("Associated ", associatedTokenAcc.toBase58());
+
+      const mintTx = await program.methods
+        .mintTo(
+          new anchor.BN(1000).mul(new anchor.BN(10).pow(new anchor.BN(2)))
+        )
+        .accounts({
+          mint: mint.publicKey,
+          token2022Program: TOKEN_2022_PROGRAM_ID,
+          associatedToken: associatedTokenAcc,
+          authority: admin.publicKey,
+        })
+        .transaction();
+
+      await sendAndConfirmTransaction({
+        connection: provider.connection,
+        transaction: mintTx,
+        signers: [admin],
+      });
+
+      const receiverAssociatedTokenAcc =
+        await createAssociatedTokenAccountIdempotent(
+          provider.connection,
+          admin,
+          mint.publicKey,
+          receiver.publicKey,
+          { commitment: "processed" },
+          TOKEN_2022_PROGRAM_ID
+        );
+
+      console.log(
+        "receiverAssociatedTokenAcc ",
+        receiverAssociatedTokenAcc.toBase58()
+      );
+
+      const transferAmount = new anchor.BN(500).mul(
+        new anchor.BN(10).pow(new anchor.BN(2))
+      );
+
+      const fee = transferAmount.muln(feeBasisPoint / 10_000);
+
+      const transferTx = await program.methods
+        .transferTo(transferAmount, fee)
+        .accounts({
+          mint: mint.publicKey,
+          token2022Program: TOKEN_2022_PROGRAM_ID,
+          fromAcc: associatedTokenAcc,
+          toAcc: receiverAssociatedTokenAcc,
+          authority: admin.publicKey,
+        })
+        .transaction();
+
+      const transfTxId = await sendAndConfirmTransaction({
+        connection: provider.connection,
+        transaction: transferTx,
+        signers: [admin],
+      });
+
+      console.log("TransferId", transfTxId);
     })
   );
 });

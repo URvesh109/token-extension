769dd16f2fac7f770a7877e5f250e558fba4e169
Urvesh109 <urveshi.t@gmail.com>
2024-02-08 22:03:46 +0530

    Add receiver 2 & 3 keypairs

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 solana airdrop --keypair keypairs/admin.json 100
 solana airdrop --keypair keypairs/receiver.json 100
+solana airdrop --keypair keypairs/receiver2.json 100
+solana airdrop --keypair keypairs/receiver3.json 100
 
 anchor build --arch sbf
 

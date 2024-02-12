39411e6142d79544603d0395a2cae748f21e2ba3
Urvesh109 <urveshi.t@gmail.com>
2024-02-12 06:00:01 +0530

    Add airdrop.sh and remove deploy.sh

--- airdrop.sh
+++ airdrop.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-solana airdrop --keypair keypairs/admin.json 100
 solana airdrop --keypair keypairs/receiver.json 100
 solana airdrop --keypair keypairs/receiver2.json 100
 solana airdrop --keypair keypairs/receiver3.json 100
-
-anchor build --arch sbf
-
-anchor deploy \
-  -p token_extension \
-  --program-keypair keypairs/deploy-key-token-extension.json

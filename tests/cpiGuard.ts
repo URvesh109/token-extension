11ebbcd4c17e0cc00be8132636c98837e4f9266b
Urvesh109 <urveshi.t@gmail.com>
2024-02-20 17:57:54 +0530

    Add test-case to validate CPI GUARD

--- tests/cpiGuard.ts
+++ tests/cpiGuard.ts
@@ -1,15 +1,23 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { TokenExtension } from "../target/types/token_extension";
+import { Opaque } from "./../target/types/opaque";
 import {
   TOKEN_2022_PROGRAM_ID,
   ExtensionType,
   createMint,
   getAccountLen,
   createEnableCpiGuardInstruction,
+  mintTo,
+  createAssociatedTokenAccountIdempotent,
 } from "@solana/spl-token";
 import * as path from "path";
-import { keypairFromFile, sendAndConfirmTransaction } from "./utils";
+import {
+  airdrop,
+  keypairFromFile,
+  rejectedWith,
+  sendAndConfirmTransaction,
+} from "./utils";
 import Debug from "debug";
 
 const log = Debug("log:cpiGuard");
@@ -22,12 +30,18 @@ describe("token-extension: cpi guard enable", () => {
   anchor.setProvider(provider);
 
   const program = anchor.workspace.TokenExtension as Program<TokenExtension>;
+  const opaqueProgram = anchor.workspace.Opaque as Program<Opaque>;
 
   const admin = keypairFromFile(path.join(__dirname, "../keypairs/admin.json"));
+  const receiver = keypairFromFile(
+    path.join(__dirname, "../keypairs/receiver.json")
+  );
 
   it("enable cpi guard account", async () => {
+    await airdrop(provider, receiver.publicKey);
+
     const tokenAccount = anchor.web3.Keypair.generate();
-    log("tokenAccount", tokenAccount.publicKey.toBase58());
+    log("TokenAccount", tokenAccount.publicKey.toBase58());
 
     const mint = anchor.web3.Keypair.generate();
     log("Mint", mint.publicKey.toBase58());
@@ -79,6 +93,119 @@ describe("token-extension: cpi guard enable", () => {
       transaction,
       signers: [admin, tokenAccount],
     });
-    log("cpi guarded txId ", txId);
+    log("Cpi guarded txId ", txId);
+
+    const mintTotxId = await mintTo(
+      provider.connection,
+      admin,
+      mint.publicKey,
+      tokenAccount.publicKey,
+      admin.publicKey,
+      20 * 10 ** decimals,
+      [],
+      { commitment: "finalized", skipPreflight: true },
+      TOKEN_2022_PROGRAM_ID
+    );
+
+    log("Minted to admin token acc txId ", mintTotxId);
+
+    // without cpi guard
+    const associatedTokenAcc = await createAssociatedTokenAccountIdempotent(
+      provider.connection,
+      admin,
+      mint.publicKey,
+      admin.publicKey,
+      { commitment: "finalized", skipPreflight: true },
+      TOKEN_2022_PROGRAM_ID
+    );
+
+    log("Admin ATA created ", associatedTokenAcc.toBase58());
+
+    const mintToAtaId = await mintTo(
+      provider.connection,
+      admin,
+      mint.publicKey,
+      associatedTokenAcc,
+      admin.publicKey,
+      20 * 10 ** decimals,
+      [],
+      { commitment: "finalized", skipPreflight: true },
+      TOKEN_2022_PROGRAM_ID
+    );
+
+    log("Minted to admint ATA txId ", mintToAtaId);
+
+    // without cpi guard
+    const receiverATA = await createAssociatedTokenAccountIdempotent(
+      provider.connection,
+      receiver,
+      mint.publicKey,
+      receiver.publicKey,
+      { commitment: "finalized", skipPreflight: true },
+      TOKEN_2022_PROGRAM_ID
+    );
+
+    log("Receiver ATA created ", receiverATA.toBase58());
+
+    const receiverBalBeforeCpi = await provider.connection.getBalance(
+      receiver.publicKey,
+      "finalized"
+    );
+
+    log("Receiver sol balance before cpi ", receiverBalBeforeCpi);
+
+    const transferTokenTx = await program.methods
+      .transferToken(new anchor.BN(2 * 10 ** decimals), decimals)
+      .accounts({
+        mint: mint.publicKey,
+        fromAcc: associatedTokenAcc,
+        toAcc: receiverATA,
+        token2022Program: TOKEN_2022_PROGRAM_ID,
+        authority: admin.publicKey,
+        opaque: opaqueProgram.programId,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        badWallet: receiver.publicKey,
+      })
+      .transaction();
+
+    const transferTokenTxId = await sendAndConfirmTransaction({
+      connection: provider.connection,
+      transaction: transferTokenTx,
+      signers: [admin],
+    });
+
+    log("Transfer token without CPI GUARD txId ", transferTokenTxId);
+
+    const receiverBalAfterCpi = await provider.connection.getBalance(
+      receiver.publicKey,
+      "finalized"
+    );
+
+    log("Receiver sol balance after cpi ", receiverBalAfterCpi);
+
+    log("Failed: Transfer token with CPI GUARD");
+
+    const transferTokenCpiGuardTx = await program.methods
+      .transferToken(new anchor.BN(2 * 10 ** decimals), decimals)
+      .accounts({
+        mint: mint.publicKey,
+        fromAcc: tokenAccount.publicKey,
+        toAcc: receiverATA,
+        token2022Program: TOKEN_2022_PROGRAM_ID,
+        authority: admin.publicKey,
+        opaque: opaqueProgram.programId,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        badWallet: receiver.publicKey,
+      })
+      .transaction();
+
+    await rejectedWith(
+      sendAndConfirmTransaction({
+        connection: provider.connection,
+        transaction: transferTokenCpiGuardTx,
+        signers: [admin],
+      }),
+      Error
+    );
   });
 });
